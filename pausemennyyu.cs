47cbcf1850e35b83cbe4535a4a69520d10bf0914
Hasan Musayev <111653544+hsmyv@users.noreply.github.com>
2022-09-17 03:00:06 +0400

    Menu -> Pause Menu
    Pause Menu is not ordinary menu for button to click. Also in this process you have to use to Time.timeScale and UI for will appears at the menu.

---
+++ pausemennyyu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemennyyu : MonoBehaviour
+{
+    public static bool GameisPaused = false;
+    public GameObject MenuUI;
+    public GameObject Menubuttonu;
+
+    public GameObject UmumiUI;
+    public AudioSource ses;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    public void Start()
+    {
+        MenuUI.SetActive(false);
+    }
+
+    public void Pausebutton1()
+    {
+        MenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Debug.Log("isledi");
+        Menubuttonu.SetActive(false);
+        UmumiUI.SetActive(false);
+        ses.Play();
+    }
+    
+     
+ public void ResumeButton()
+    {
+        MenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Menubuttonu.SetActive(true);
+        Debug.Log("islediresume");
+        UmumiUI.SetActive(true);
+        ses.Stop();
+    }
+
+
+}
